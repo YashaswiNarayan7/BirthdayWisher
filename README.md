61bbc198003c69e274a96a213070fb8a6fbffd90
Yashaswi N <yashaswi7504@gmail.com>
2025-06-12 20:31:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Made with React with Hooks.
 
 ## Getting started
 
-You can view a live demo over at  https://yashaswinarayan7.github.io/ChronoWish
+You can view a live demo over at  https://yashaswinarayan7.github.io/BirthdayWisher/
 
 To get It running locally:
 
---
+++ precache-manifest.ea70409bb64f2a77129143b27b2e6803.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1379ba27fb452045004ea4ae8614c201",
+    "url": "/ChronoWish/index.html"
+  },
+  {
+    "revision": "825a3aecb59ab9a3b72b",
+    "url": "/ChronoWish/static/css/main.26086a97.chunk.css"
+  },
+  {
+    "revision": "6897e6e999e3d9389d8d",
+    "url": "/ChronoWish/static/js/2.abd8a5b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ChronoWish/static/js/2.abd8a5b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825a3aecb59ab9a3b72b",
+    "url": "/ChronoWish/static/js/main.6993f299.chunk.js"
+  },
+  {
+    "revision": "47e75ad5e79d0344d27d",
+    "url": "/ChronoWish/static/js/runtime-main.c9d5fe75.js"
+  },
+  {
+    "revision": "90118e04a5ab527cb88749db824d7b7b",
+    "url": "/ChronoWish/static/media/githubLogo.90118e04.svg"
+  }
+]);
