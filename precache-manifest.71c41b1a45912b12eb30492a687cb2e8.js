29bf8711ec576d2e7e2fb4fe359e59edbf890dc5
YashaswiNarayan7 <yashaswi7504@gmail.com>
2025-06-12 18:08:45 +0530

    Updates

---
+++ precache-manifest.71c41b1a45912b12eb30492a687cb2e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3571ce4b76c370a0404d46ea2612ee",
+    "url": "/BirthdayWisher/index.html"
+  },
+  {
+    "revision": "2dbdd42667781da1b47d",
+    "url": "/BirthdayWisher/static/css/main.26086a97.chunk.css"
+  },
+  {
+    "revision": "305f28775140e625e745",
+    "url": "/BirthdayWisher/static/js/2.728e76d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BirthdayWisher/static/js/2.728e76d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dbdd42667781da1b47d",
+    "url": "/BirthdayWisher/static/js/main.39685b58.chunk.js"
+  },
+  {
+    "revision": "942e4fc1432265a8cff0",
+    "url": "/BirthdayWisher/static/js/runtime-main.ea5c21bd.js"
+  },
+  {
+    "revision": "90118e04a5ab527cb88749db824d7b7b",
+    "url": "/BirthdayWisher/static/media/githubLogo.90118e04.svg"
+  }
+]);
